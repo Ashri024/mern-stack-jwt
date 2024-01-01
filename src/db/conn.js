88039bb6d719fe8924e9b9787a31f027f8835033
Ashri024 <ashri.2214504823@mujonline.edu.in>
2024-01-01 15:55:36 +0530

    fourth

--- src/db/conn.js
+++ src/db/conn.js
@@ -1,5 +1,5 @@
 const mongoose= require("mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/signUpForm").then(()=>{
+mongoose.connect(`${process.env.DB_URL}`).then(()=>{
     console.log("connected successfully!!");
 }).catch(e=>{
     console.log("Error: ", e);
